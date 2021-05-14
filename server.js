b1483c2d54b674a00f2a4efe7e9116bcc2ba3843
John <john.rybo@gmail.com>
2021-05-14 11:19:46 +0200

    fix

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, '../../build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
-app.get('/', (req, res, next) => res.sendFile(__dirname + './index.html'));
+app.get('/', (req, res, next) => res.sendFile(__dirname + './client/index.html'));
 
 const rooms = [];
 
